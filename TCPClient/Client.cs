651443c10fbb50654dcd9222489da1dbe9f653d7
beatHunteRcode <nikitasherepa@gmail.com>
2021-10-14 00:57:07 +0300

    Initial commit

---
+++ TCPClient/Client.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TCPClient
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                const string ip = "127.0.0.1";
+                const int port = 0451;
+
+                Console.Write("Who are you?: ");
+                User clientUser = new User(Console.ReadLine());
+                User serverUser = new User();
+                IPEndPoint TCPEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+                Socket TCPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Connecting to the TCPChat...");
+                do
+                {
+                    try
+                    {
+
+                        TCPSocket.Connect(TCPEndPoint);
+                    }
+                    catch (SocketException)
+                    {
+                        Console.WriteLine("No adress to connect. Reconnecting...");
+                    }
+                }
+                while (!TCPSocket.Connected);
+
+                Console.WriteLine("Connected.");
+                Console.WriteLine($"Hi, {clientUser.Name}. Welcome to the TCPChat! Type 'exit()' to quit the TCPChat.");
+                Console.WriteLine("");
+                var data = new byte[256];
+                string json = JsonConvert.SerializeObject(clientUser);
+                data = Encoding.UTF8.GetBytes(json);
+                TCPSocket.Send(data);
+
+                Thread receiveThread = new Thread(() =>
+                {
+                    byte[] buffer = new byte[256];
+                    int bufferSize = 0;
+                    StringBuilder sb = new StringBuilder();
+                    while (true)
+                    {
+                        sb.Clear();
+                        Array.Clear(buffer, 0, buffer.Length);
+                        bufferSize = TCPSocket.Receive(buffer);
+                        sb.Append(Encoding.UTF8.GetString(buffer, 0, bufferSize));
+                        serverUser = JsonConvert.DeserializeObject<User>(sb.ToString());
+                        Console.WriteLine($"[{getCurrentTime()}] {serverUser.Name}: {serverUser.Message}");
+                    }
+                });
+                receiveThread.Start();
+
+                do
+                {
+                    clientUser.Message = Console.ReadLine();
+                    json = JsonConvert.SerializeObject(clientUser);
+                    data = Encoding.UTF8.GetBytes(json);
+                    TCPSocket.Send(data);
+                }
+                while (clientUser.Message != "exit()");
+                Console.WriteLine("Disconnected.");
+                TCPSocket.Shutdown(SocketShutdown.Both);
+                TCPSocket.Close();
+
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static string getCurrentTime()
+        {
+            return DateTime.Now.ToString("HH:mm");
+        }
+
+    }
+}
---
+++ TCPServer/Server.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TCPChat
+{
+    class Server
+    {       
+        const string ip = "127.0.0.1";
+        const int port = 0451;
+        const int listenersNumb = 10;
+        private static List<Socket> listenersList = new List<Socket>();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint TCPEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+                Socket TCPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                TCPSocket.Bind(TCPEndPoint);
+
+                TCPSocket.Listen(listenersNumb);
+                while (true)
+                {
+                    var listener = TCPSocket.Accept();
+                    listenersList.Add(listener);
+                    Thread thread = new Thread(() =>
+                    {
+                        byte[] buffer = new byte[256];
+                        int bufferSize = 0;
+                        StringBuilder data = new StringBuilder();
+                        User user = new User();
+                        welcomeMessage(listener, buffer, bufferSize, data, ref user);
+                        broadcastNewUserMessage(listenersList, listener, user);
+                        while (listener.Connected)
+                        {
+                            try
+                            {
+                                do
+                                {
+                                    data.Clear();
+                                    Array.Clear(buffer, 0, buffer.Length);
+                                    bufferSize = listener.Receive(buffer);
+                                    data.Append(Encoding.UTF8.GetString(buffer, 0, bufferSize));
+                                    user = JsonConvert.DeserializeObject<User>(data.ToString());
+                                    Console.WriteLine($"[{getCurrentTime()}] {user.Name}: {user.Message}");
+
+                                    broadcastMessage(listener, buffer);
+                                }
+                                while (listener.Available > 0);
+                            }
+                            catch
+                            {
+                                disconnectMessage(listener, user);
+                            }
+
+                        }
+                    });
+                    thread.Start();
+                }
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        public static void welcomeMessage(Socket listener, byte[] buffer, int bufferSize, StringBuilder data, ref User user)
+        {
+            data.Clear();
+            bufferSize = listener.Receive(buffer);
+            data.Append(Encoding.UTF8.GetString(buffer, 0, bufferSize));
+            user = JsonConvert.DeserializeObject<User>(data.ToString());
+            Console.WriteLine($"[{getCurrentTime()}] {user.Name} has been connected.");
+        }
+
+        private static void broadcastNewUserMessage(List<Socket> listenersList, Socket currentListener, User user)
+        {
+            foreach (Socket listener in listenersList)
+            {
+                if (!listener.Equals(currentListener))
+                {
+                    user.Message = "has been connected.";
+                    string json = JsonConvert.SerializeObject(user);
+                    var data = Encoding.UTF8.GetBytes(json);
+                    listener.Send(data);
+                }
+            }
+        }
+
+        private static void broadcastMessage(Socket currentListener, byte[] data)
+        {
+            foreach (Socket listener in listenersList)
+            {
+                if (!listener.Equals(currentListener)) listener.Send(data);
+            }
+        }
+
+        public static void disconnectMessage(Socket listener, User user)
+        {
+            Console.WriteLine($"{user.Name} has been disconnected.");
+            listenersList.Remove(listener);
+        }
+
+        private static string getCurrentTime()
+        {
+            return DateTime.Now.ToString("HH:mm");
+        }
+    }
+}
